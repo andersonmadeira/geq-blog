2c91ad8bf0c6ff08e0aaaa5d2ca3fea7168b44f4
Anderson Madeira <andersonmadeiracs@gmail.com>
2016-06-25 14:31:35 -0300

    Corrigidos paths
    TODO: Configurar o bower pra colocar os arquivos em pastas espefícicas. Focar agora na implementação e usabilidade.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,17 +8,17 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="shortcut icon" href="{{ asset('blog.ico') }}" />
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('bower_components/AdminLTE/bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('bower_components/AdminLTE/dist/css/AdminLTE.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/main.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{ asset('css/skins/skin-blue.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('bower_components/AdminLTE/dist/css/skins/skin-blue.min.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -132,17 +132,15 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.0 -->
-<script src="{{ asset('js/plugins/jQuery/jQuery-2.2.0.min.js') }}"></script>
+<script src="{{ asset('bower_components/AdminLTE/plugins/jQuery/jQuery-2.2.0.min.js') }}"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ asset('bower_components/AdminLTE/bootstrap/js/bootstrap.min.js') }}"></script>
 <!-- SlimScroll -->
-<script src="{{ asset('js/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('bower_components/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <!-- FastClick -->
-<script src="{{ asset('js/plugins/fastclick/fastclick.js') }}"></script>
+<script src="{{ asset('bower_components/AdminLTE/plugins/fastclick/fastclick.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('js/app.min.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('js/demo.js') }}"></script>
+<script src="{{ asset('bower_components/AdminLTE/dist/js/app.min.js') }}"></script>
 
 @yield('scripts')
 
